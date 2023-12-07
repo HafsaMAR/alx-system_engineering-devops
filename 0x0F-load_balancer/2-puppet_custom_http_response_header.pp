36ee8a303a5958c51d05cc0149062eaa3b8ab2df
HafsaMAR <marouche.hafsa@gmail.com>
2023-12-07 22:24:59 +0100

    fix error

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,18 @@
 # Install and configure nginx
 
-class nginx {
-    exec { 'update':
-        command => '/user/bin/apt-get update',
-    }
-    -> package { 'nginx':
-        ensure => installed,
-    }
+exec { 'update':
+    command => '/user/bin/apt-get update',
+}
+-> package { 'nginx':
+    ensure => installed,
+}
 
-    file_line {'header_served_by':
-        path  => '/etc/nginx/sites-available/default',
-        match => '^server {',
-        line  => "server {\n\tadd_header X-Served-By \"$(hostnama)\";",
-        multiple => false,  
-    }
-    -> exec { 'run':
+file_line {'header_served_by':
+    path  => '/etc/nginx/sites-available/default',
+    match => '^server {',
+    line  => "server {\n\tadd_header X-Served-By \"$(hostnama)\";",
+    multiple => false,  
+}
+-> exec { 'run':
         command => '/user/sbin/service nginx restart',
-    }
 }
