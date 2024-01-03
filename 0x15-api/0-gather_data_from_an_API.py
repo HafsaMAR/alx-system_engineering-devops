8581bd606ee2a2ca4d3d916d1eac0a31b8ef5b86
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-03 01:17:23 +0100

    Task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-"""
-Checks student output for returning info from REST API
-"""
+""" TAsk0 model """
 
 import requests
-import sys
-
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
-
-    resp = requests.get(todos_url).json()
-
-    filename = 'student_output'
-    count = 0
-    with open(filename, 'r') as f:
-        next(f)
-        for line in f:
-            count += 1
-            if line[0] is '\t' and line[1] is ' ' and line[-1] is '\n':
-                print("Task {} Formatting: OK".format(count))
-            else:
-                print("Task {} Formatting: Incorrect".format(count))
+from sys import argv
 
 
 if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    # task 0 function
+    id = argv[1]
+    params_todo = {"userId": id}
+    params_user = {"id": id}
+    url_todo = "https://jsonplaceholder.typicode.com/todos"
+    url_user = "https://jsonplaceholder.typicode.com/users"
+
+    response_todo = requests.get(url_todo, params=params_todo)
+    data_todo = response_todo.json()
+
+    response_user = requests.get(url_user, params=params_user)
+    data_user = response_user.json()
+
+    def todo_true(list):
+        n = 0
+        for dic in list:
+            if dic.get("completed"):
+                n += 1
+        return (n)
+
+    tr = todo_true(data_todo)
+    name = data_user[0].get("name")
+
+    output = f"Employee {name} is done with tasks({tr}/{len(data_todo)}):"
+    print(output)
+
+    for dic in data_todo:
+        if dic.get("completed"):
+            print(f" \t{dic.get('title')}")
