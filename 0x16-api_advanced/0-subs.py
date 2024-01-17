110fc544e8fc3f67485d23e12ecda463624a828e
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-17 17:12:12 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """Construct the URL for the subreddit's about page in JSON format"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
     }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
