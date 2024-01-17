21e1f7fff6ed3d2985d962201be989ae811535ce
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-17 17:03:50 +0100

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,18 +9,19 @@ import requests
 def number_of_subscribers(subreddit):
     """Construct the URL for the subreddit's about page in JSON format"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "app/1.0"}
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+
+    else:
+        data = response.json().get("data")
+
+        return data.get("subscribers")
+
     # response = requests.get(url, headers={'User-Agent': 'app/1.0'})
     # if response.status_code == 200:
     #     js_data = response.json()
