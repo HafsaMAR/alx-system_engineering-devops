aa555e11bc6cc23d804052d3f0b512d1d3ec34df
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-17 17:41:41 +0100

    code update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,29 +15,10 @@ def number_of_subscribers(subreddit):
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-
-        data = response.json().get("data")
-        return data.get("subscribers")
-    except requests.HTTPError as e:
-        if e.response.status_code == 404:
-            return 0
-        else:
-            return None
-    except requests.RequestException as e:
-        return None
-    # if response.status_code == 404:
-    #     return 0
-
-    # else:
-    #     data = response.json().get("data")
-
-    #     return data.get("subscribers")
-
-    # response = requests.get(url, headers={'User-Agent': 'app/1.0'})
-    # if response.status_code == 200:
-    #     js_data = response.json()
-    #     subscribers = js_data['data']['subscribers']
-    #     return subscribers
-    # else:
-    #     return 0
+        print(response)
+        print(response.ok)
+        print("**********************************************************************")
+        if response.status_code == 200:
+            data = response.json().get("data")
+            return data.get("subscribers")
+        return 0
