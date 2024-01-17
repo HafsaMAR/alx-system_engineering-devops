6199a29e57d074e540e3ec4b551687fc48e63b92
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-17 16:32:43 +0100

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,17 +2,30 @@
 """function that queries the Reddit API and prints the titles of the
 first 10 hot posts listed for a given subreddit."""
 
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    response = requests.get(url, headers={"User_Agent": "app/1.0"})
-    data = response.json()
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
-    if response.status_code == 200:
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['tittle'])
-    else:
-        print(None)
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
