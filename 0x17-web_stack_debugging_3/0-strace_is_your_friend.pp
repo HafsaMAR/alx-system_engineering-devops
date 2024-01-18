58fbff5684488fbe08703674eaf8e76c86fb66c0
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-18 02:03:18 +0100

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 exec { 'fix error':
-  provider => 'shell',
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+
+  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
+  path     => '/usr/local/bin/:/bin/'
 }
