769275f0912b86c5890fd06c87164baedc644866
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-18 13:14:46 +0100

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Using strace, find out why Apache is returning a 500 error.
 
-exec { 'fix error':
-  provider => 'shell',
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-  path     => '/usr/local/bin/:/bin/'
+exec { 'fix-typo':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
