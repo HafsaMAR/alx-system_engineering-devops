016feaf351e4bde5c5903d377b2c19eec6960844
HafsaMAR <marouche.hafsa@gmail.com>
2024-01-25 22:35:07 +0100

    update

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # Modify the Ulimit of the /etc/default/nginx for the server to handle traffic
 
 # Increase the ULIMIT of the nginx server
-exec { 'fix--for-nginx':
-	command => '/bin/sed -i "s/15/4096" /etc/default/nginx',
-	path => '/usr/local/bin:/bin/',
+exec { 'update ULIMIT':
+	command  => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 20000\"/' /etc/default/nginx",
+	provider => shell,
 }
 
 exec { 'nginx-restart':
-	command => '/etc/init.d/nginx restart',
-	path 	=> '/etc/init.d',
+	command  => 'service nginx restart',
+	provider => 'shell',
 }
